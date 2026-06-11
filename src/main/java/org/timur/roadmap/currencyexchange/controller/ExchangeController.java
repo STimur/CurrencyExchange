fa5d0ccb7e24a0f33d88166e6499bc3e56fa874a
Timur <timur.salakhetdinov@gmail.com>
2026-06-11 22:32:51 +0300

    implemented controller for exchange currency case

---
+++ src/main/java/org/timur/roadmap/currencyexchange/controller/ExchangeController.java
@@ -0,0 +1,44 @@
+package org.timur.roadmap.currencyexchange.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.timur.roadmap.currencyexchange.dao.CurrencyDao;
+import org.timur.roadmap.currencyexchange.dao.ExchangeRateDao;
+import org.timur.roadmap.currencyexchange.model.Conversion;
+import org.timur.roadmap.currencyexchange.service.ExchangeRateService;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+@WebServlet("/exchange")
+public class ExchangeController extends HttpServlet {
+    private final ExchangeRateService exchangeRateService;
+    private final ObjectMapper mapper;
+
+    public ExchangeController() {
+        this.exchangeRateService = new ExchangeRateService(new ExchangeRateDao(), new CurrencyDao());
+        this.mapper = new ObjectMapper();
+    }
+
+    public ExchangeController(ExchangeRateService exchangeRateService, ObjectMapper mapper) {
+        this.exchangeRateService = exchangeRateService;
+        this.mapper = mapper;
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String baseCurrencyCode = req.getParameter("from");
+        String targetCurrencyCode = req.getParameter("to");
+        String amount = req.getParameter("amount");
+
+        Conversion conversion =
+                exchangeRateService.convert(baseCurrencyCode, targetCurrencyCode, new BigDecimal(amount));
+
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.setContentType("application/json");
+        mapper.writeValue(resp.getWriter(), conversion);
+    }
+}
---
+++ src/test/java/org/timur/roadmap/currencyexchange/controller/ExchangeControllerTest.java
@@ -0,0 +1,59 @@
+package org.timur.roadmap.currencyexchange.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.timur.roadmap.currencyexchange.model.Conversion;
+import org.timur.roadmap.currencyexchange.service.ExchangeRateService;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ExchangeControllerTest {
+
+    @Mock
+    private ExchangeRateService exchangeRateServiceMock;
+    @Mock
+    private ObjectMapper mapperMock;
+    @Mock
+    private HttpServletRequest requestMock;
+    @Mock
+    private HttpServletResponse responseMock;
+    @Mock
+    private PrintWriter writerMock;
+
+    @InjectMocks
+    private ExchangeController exchangeController;
+
+    @BeforeEach
+    void setup() throws IOException {
+        when(responseMock.getWriter()).thenReturn(writerMock);
+    }
+
+    @Test
+    void shouldReturnConversion() throws IOException {
+        Conversion conversion = new Conversion(null, null, null, null, null);
+        when(requestMock.getParameter("from")).thenReturn("USD");
+        when(requestMock.getParameter("to")).thenReturn("AUD");
+        when(requestMock.getParameter("amount")).thenReturn("10");
+        when(exchangeRateServiceMock.convert("USD", "AUD", new BigDecimal("10"))).thenReturn(conversion);
+
+        exchangeController.doGet(requestMock, responseMock);
+
+        verify(exchangeRateServiceMock).convert("USD", "AUD", new BigDecimal("10"));
+        verify(responseMock).setStatus(HttpServletResponse.SC_OK);
+        verify(responseMock).setContentType("application/json");
+        verify(mapperMock).writeValue(writerMock, conversion);
+    }
+}
